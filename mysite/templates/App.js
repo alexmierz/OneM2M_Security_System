f4717d9dde9dde28fb4a729019c800fc6b6a19dc
reassign <snowboardz125@gmail.com>
2023-04-01 16:02:11 -0400

    update

---
+++ mysite/templates/App.js
@@ -0,0 +1,20 @@
+import React from "react";
+import "./App.css";
+import DashboardComponent from "./Dashboard.js";
+
+function App() {
+  /*return (
+     <DashboardComponent 
+         defaultCSE="http://34.238.135.110:8080"
+         defaultOriginator="Cdashboard" 
+         defaultRI="id-in" />
+  );*/
+  return (
+     <DashboardComponent 
+         defaultCSE="http://35.89.20.163:8080"
+         defaultOriginator="Cdashboard" 
+         defaultRI="id-in" />
+  );
+}
+
+export default App;
