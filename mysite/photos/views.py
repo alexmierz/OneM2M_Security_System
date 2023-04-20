51a4dc12d26c58f347c33ef89e4db6437b14372f
reassign <snowboardz125@gmail.com>
2023-04-19 21:59:04 -0400

    problemas

--- mysite/photos/views.py
+++ mysite/photos/views.py
@@ -1,27 +1,47 @@
 from django.shortcuts import render
 from .forms import PhotoUpload
-
+import numpy as np
+import os.path
 import binascii
+import face_recognition
+import face_recognition as faceRegLib
+import dlib
 
 def photo_upload_view(request):
+    
     if request.method == "POST":
         form = PhotoUpload(request.POST, request.FILES)
         if form.is_valid():
             # handle_uploaded_file(request.FILES.get("file"))
             form.save()
+            folder_path ="./media/images"
+            recent = 0
+            recent_file = None
+            for entry in os.scandir(folder_path):
+                if entry.is_file():
+                    mod_time = entry.stat().st_mtime_ns
+                    if mod_time > recent:
+                        recent_file = entry.name
+                        recent = mod_time
+            print(recent_file)
+            file_path = folder_path +"/" + str(recent_file)
+            encode(file_path)
+           
             return render(request, "home.html")
         print("success!")
     else:
         form = PhotoUpload()
     return render(request, "upload.html", {"form": form})
 
-def handle_uploaded_file(f):
-    with open("some/file/name.txt", "wb+") as destination:
-        for chunk in f.chunks():
-            destination.write(chunk)
+def encode(f):
+    ab_image = face_recognition.load_image_file(f)
+    ab_encoding = face_recognition.face_encodings(ab_image)[0]
+    np.savetxt("abencoidng.txt",ab_encoding)
+    with open('abencoding.txt', 'rb') as fp:
+        hexstring = binascii.hexlify(fp.read())
 
-def convert_to_hex(image):
-    with image.open('rb') as f:
-        hex_data = binascii.hexlify(f.read())
-    return hex_data.decode('utf-8')
+    print(hexstring)
 
+    with open('ab.text','wb') as fp:
+        b = np.loadtxt("ab.txt")
+    print(b)
